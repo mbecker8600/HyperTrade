045a344c428477b097576fa7fdbb3ec547cca4ca
Michael Becker <michaelbecker022@gmail.com>
2025-01-03 15:55:39 +0000

    Adding more tests

--- code/trading/finance/tests/commission_tests.py
+++ code/trading/finance/tests/commission_tests.py
@@ -5,7 +5,7 @@ from code.trading.finance.commission import NoCommission
 
 class TestNoCommisionModel(unittest.TestCase):
 
-    def test_no_commision_returns_zero(self):
+    def test_no_commision_returns_zero(self) -> None:
         """NoCommission Commision Model should return zero for all trades"""
 
-        self.assertEqual(NoCommission().calculate(None, None), 0.0)
+        self.assertEqual(NoCommission.calculate(None, None), 0.0)
